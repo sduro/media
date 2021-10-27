8a99b66155d2d1697cdf70ded09e120eaaa33a16
sduro <sergiduro@gmail.com>
2021-10-27 18:38:54 +0200

    new file

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from os import linesep
 import sqlite3
 
-conn = sqlite3.connect('MyVideos116.db')
-print ('Database opened successfully...')
 
-cursor = conn.execute("select * from movie")
-for row in cursor:
-    print ("Title: ", row[2],"Year: ", row[28][0:4])
 
-print ("Done")
-conn.close()
+def read_local_files():
+    with open('/home/sduro/Documentos/llista.txt') as f:
+        for line in f:
+            print (line[0:-1])
+    f.close
 
-   
-#conn.commit()
+def read_kodi_database():
+    conn = sqlite3.connect('MyVideos116.db')
+    print ('Database opened successfully...')
+    c22='642'
+    movie = '%Tenet.(2020).mkv'
+    cursor = conn.execute("select * from movie where c22 like ?",(movie,))
+    for row in cursor:
+        print (row[2]," | ", row[28][0:4]," | ", row[24])
+
+    print ("Done")
+    conn.close()
+
+#read_local_files()
+read_kodi_database()
+
 
